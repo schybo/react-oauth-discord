de0032bde754d6d9db80ca9ac77a844b7704f566
Brent Scheibelhut <brent.scheibelhut@instacart.com>
2023-02-26 07:32:14 -0500

    Built

--- dist/DiscordButton.js
+++ dist/DiscordButton.js
@@ -14,8 +14,8 @@ require("core-js/modules/es.string.search.js");
 var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 require("./discordButton.css");
-const _excluded = ["discordClientId", "redirectUri", "responseType", "scopes", "debug", "primary", "backgroundColor", "size", "label"],
-  _excluded2 = ["primary", "backgroundColor", "size", "label"];
+const _excluded = ["discordClientId", "redirectUri", "responseType", "scopes", "debug", "backgroundColor", "size", "label", "onClick", "newTab"],
+  _excluded2 = ["primary", "backgroundColor", "size", "label", "newTab"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
@@ -33,10 +33,11 @@ const DiscordLogin = _ref => {
       responseType = 'code',
       scopes = ['identify', 'email'],
       debug = false,
-      primary,
       backgroundColor,
       size,
-      label
+      label,
+      onClick,
+      newTab = false
     } = _ref,
     props = _objectWithoutProperties(_ref, _excluded);
   const params = new URLSearchParams({
@@ -53,18 +54,15 @@ const DiscordLogin = _ref => {
   if (debug) {
     console.log("Final URL: ".concat(url, "}"));
   }
-
-  /* box-shadow: none;
-    border-color: #d2e3fc;
-    outline: none; hover */
-
-  const mode = primary ? 'discord-button--primary' : 'discord-button--secondary';
-  return /*#__PURE__*/_react.default.createElement("a", {
-    href: url,
-    className: 'discord-link'
-  }, /*#__PURE__*/_react.default.createElement("button", _extends({
+  const defaultOnClick = e => {
+    e.preventDefault();
+    // window.location.href = url
+    newTab ? window.open(url, '_blank') : window.open(url);
+  };
+  return /*#__PURE__*/_react.default.createElement("button", _extends({
     type: "button",
-    className: ['discord-button', "discord-button--".concat(size), mode].join(' '),
+    onClick: onClick || defaultOnClick,
+    className: ['discord-button', "discord-button--".concat(size)].join(' '),
     style: backgroundColor && {
       backgroundColor
     }
@@ -85,7 +83,7 @@ const DiscordLogin = _ref => {
   }), /*#__PURE__*/_react.default.createElement("path", {
     className: "st0",
     d: "M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"
-  }))), /*#__PURE__*/_react.default.createElement("span", null, label || 'Sign in with Discord')));
+  }))), /*#__PURE__*/_react.default.createElement("span", null, label && label != '' ? label : 'Sign in with Discord'));
 };
 
 /**
@@ -97,22 +95,23 @@ const DiscordButtonStory = _ref2 => {
       primary,
       backgroundColor,
       size,
-      label
+      label,
+      newTab
     } = _ref2,
     props = _objectWithoutProperties(_ref2, _excluded2);
   return /*#__PURE__*/_react.default.createElement(DiscordLogin, {
     discordClientId: DISCORD_CLIENT_ID,
     redirectUri: DISCORD_REDIRECT_URI,
     debug: true,
+    backgroundColor: backgroundColor,
+    size: size,
+    label: label,
+    newTab: newTab,
     props: props
   });
 };
 exports.DiscordButtonStory = DiscordButtonStory;
 DiscordButtonStory.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: _propTypes.default.bool,
   /**
    * What background color to use
    */
@@ -128,11 +127,40 @@ DiscordButtonStory.propTypes = {
   /**
    * Optional click handler
    */
-  onClick: _propTypes.default.func
+  onClick: _propTypes.default.func,
+  /**
+   * Provides some helpful logs to the console
+   */
+  debug: _propTypes.default.bool,
+  /**
+   * Whether to open the oAuth box in a new tab
+   */
+  newTab: _propTypes.default.bool,
+  /**
+   * Your Discord Client Id. Find yours at: https://discord.com/developers/applications > Application > OAuth2
+   */
+  discordClientId: _propTypes.default.string,
+  /**
+   * URL to redirect too after success or failure of user auth. Will attach discord code to be used in follow up
+   * api requests as the query parameter `code`. Has to match one of the URLs
+   * under https://discord.com/developers/applications > Application > OAuth2
+   * If doing server auth Authorization flow, redirect to your server endpoint handling the rest of the login
+   * Otherwise you can redirect to your and handle there
+   */
+  redirectUri: _propTypes.default.string,
+  /**
+   * The response type from the Discord oAuth. Defaults to `code`
+   */
+  responseType: _propTypes.default.string,
+  /**
+   * String array of scopes you need access to. Identify should always be applied, and if you're doing the
+   * Authorization flow you'll probably need `identify` as well. Default is ['identify', 'email']
+   */
+  newTab: [_propTypes.default.string]
 };
 DiscordButtonStory.defaultProps = {
   backgroundColor: null,
-  primary: false,
   size: 'medium',
-  onClick: undefined
+  onClick: undefined,
+  debug: false
 };
